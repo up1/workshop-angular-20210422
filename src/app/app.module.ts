02af2ba7d61cc414add0d66d5c503bbb3f524e30
up1 <somkiat.p@gmail.com>
2021-04-22 14:28:49 +0700

    Add custom pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { XyzComponent } from './login/xyz.component';
+import { MyUpperCasePipe } from './my-upper-case.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    XyzComponent
+    XyzComponent,
+    MyUpperCasePipe
   ],
   imports: [
     BrowserModule,
